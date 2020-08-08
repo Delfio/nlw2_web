5485ee4bf9ebeab28320924c4b54b76bcccd3de3
Delfio <delfio_eu@hotmail.com>
2020-08-08 15:37:32 -0400

    formulario inicial

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -8,7 +8,22 @@ const TeacherList: React.FC = () => {
   return (
     <div id="page-teacher-list" className="container">
         <Header title="Este são os proffys disponíveis">
+            <form id="search-teachers">
+                <div className="input-block">
+                    <label htmlFor="subject">Matéria</label>
+                    <input type="text" id="subject" />
+                </div>
 
+                <div className="input-block">
+                    <label htmlFor="week-day">Dia da semana</label>
+                    <input type="text" id="week-day" />
+                </div>
+
+                <div className="input-block">
+                    <label htmlFor="time">Hora</label>
+                    <input type="text" id="time" />
+                </div>
+            </form>
         </Header>
     </div>
   );
